42bd65b5ef4dffa7b3854e7e6c6dedec9ecb5739
Karlis Lukstins <karlis.lukstins@gmail.com>
2014-05-13 15:34:46 +0200

    Fix user premisions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  before_filter :authorize
+  before_filter :authorize, only: [:index, :show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
   def index
